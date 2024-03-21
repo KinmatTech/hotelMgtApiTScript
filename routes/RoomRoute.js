03dcb8097436de30e56ede9d371952e9144fba5e
github_KinmatTech <mathew.ani@unn.edu.ng>
2024-03-21 21:22:33 +0100

    https://github.com/KinmatTech/hotelMgtApi.git

--- routes/RoomRoute.js
+++ routes/RoomRoute.js
@@ -7,14 +7,14 @@ import {
     deleteRooms,
     fetchRooms
 } from "../controllers/RoomController.js";
- 
+import { authenticateUser, authorizeAdmin } from "../middlewares/auth.js";
+import { validateUserData } from "../middlewares/validate.js";
+
 const router = express.Router();
- 
 router.get('/rooms', getRooms);
 router.get('/rooms/:id', getRoomsById);
-router.post('/rooms', saveRooms);
-router.post('/rooms', fetchRooms);
-router.patch('/rooms/:id', updateRooms);
-router.delete('/rooms/:id', deleteRooms);
- 
+router.post('/rooms', authenticateUser, authorizeAdmin, validateUserData, saveRooms);
+router.get('/rooms/search', fetchRooms);
+router.patch('/rooms/:id', authenticateUser, authorizeAdmin, validateUserData, updateRooms);
+router.delete('/rooms/:id', authenticateUser, authorizeAdmin, deleteRooms);
 export default router;
